1329b2d19fdf6828ecc8de511851253c57aa2dbc
DaanVanYperen <daan@mostlyoriginal.net>
2017-05-17 20:02:43 +0200

    Add logo to asset loader.

--- html/src/net/mostlyoriginal/game/client/HtmlLauncher.java
+++ html/src/net/mostlyoriginal/game/client/HtmlLauncher.java
@@ -3,6 +3,10 @@ package net.mostlyoriginal.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.*;
 import net.mostlyoriginal.game.GdxArtemisGame;
 import net.mostlyoriginal.game.component.G;
 
@@ -17,5 +21,36 @@ public class HtmlLauncher extends GwtApplication {
     @Override
     public ApplicationListener createApplicationListener() {
         return new GdxArtemisGame();
+    }
+
+    @Override
+    public Preloader.PreloaderCallback getPreloaderCallback() {
+        final VerticalPanel preloaderPanel = new VerticalPanel();
+        preloaderPanel.setStyleName("gdx-preloader");
+        preloaderPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+        final Image logo = new Image(GWT.getModuleBaseURL() + "logo.gif");
+        logo.setStyleName("logo");
+        preloaderPanel.add(logo);
+        final Panel meterPanel = new SimplePanel();
+        meterPanel.setStyleName("gdx-meter");
+        meterPanel.addStyleName("red");
+        final InlineHTML meter = new InlineHTML();
+        final Style meterStyle = meter.getElement().getStyle();
+        meterStyle.setWidth(0, Style.Unit.PCT);
+        meterPanel.add(meter);
+        preloaderPanel.add(meterPanel);
+        getRootPanel().add(preloaderPanel);
+        return new Preloader.PreloaderCallback() {
+            @Override
+            public void error(String file) {
+                System.out.println("error: " + file);
+            }
+
+            @Override
+            public void update(Preloader.PreloaderState state) {
+                meterStyle.setWidth(100f * state.getProgress(), Style.Unit.PCT);
+            }
+
+        };
     }
 }
